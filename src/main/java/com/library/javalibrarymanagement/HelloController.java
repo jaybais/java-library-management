1f81bc1d847cf08fa9038dbf7cc0803e980a63bf
Jay Bais <81273683+jaybais@users.noreply.github.com>
2026-06-12 18:22:43 -0400

    Add HelloController for Phase 1 sanity check

--- src/main/java/com/library/javalibrarymanagement/HelloController.java
+++ src/main/java/com/library/javalibrarymanagement/HelloController.java
@@ -1,4 +1,4 @@
-package com.library;
+package com.library.javalibrarymanagement;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
