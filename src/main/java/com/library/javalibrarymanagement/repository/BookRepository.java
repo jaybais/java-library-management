62112f77cf6026b84e6999eb47d2a28e4eabbaab
Jay Bais <81273683+jaybais@users.noreply.github.com>
2026-06-15 09:19:20 -0400

    Phase 3 complete: BookRepository with custom query methods

--- src/main/java/com/library/javalibrarymanagement/repository/BookRepository.java
+++ src/main/java/com/library/javalibrarymanagement/repository/BookRepository.java
@@ -2,10 +2,16 @@ package com.library.javalibrarymanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.library.javalibrarymanagement.model.Book;
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    List<Book> findByAvailable(boolean available);
 
 }
