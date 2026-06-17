124700fab7b6dc335e4c431f34ecc1a8f03d8604
Jay Bais <81273683+jaybais@users.noreply.github.com>
2026-06-17 10:35:04 -0400

    Phase 5: Add BookController with REST endpoints, remove findByAvailable from BookRepository

--- src/main/java/com/library/javalibrarymanagement/repository/BookRepository.java
+++ src/main/java/com/library/javalibrarymanagement/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.library.javalibrarymanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.library.javalibrarymanagement.model.Book;
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -12,6 +14,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByTitle(String title);
 
-    List<Book> findByAvailable(boolean available);
+    // List<Book> findByAvailable(boolean available); // TODO: uncomment when available field is added to Book entity in Phase 4b
 
 }
